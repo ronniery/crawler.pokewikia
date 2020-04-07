4301b63e3ca5093914af7b30c45710467e150fa6
Ronniery Borges <ronnisti@gmail.com>
2020-04-07 20:53:42 -0300

    Updated code, added editor config and started migrate react from babel stand alone to webpack trasnpiller

--- crawler/selectors.js
+++ crawler/selectors.js
@@ -1,44 +1,44 @@
-const _ =  require('lodash.first')
+const _ = require('lodash')
 
 module.exports = ($) => {
-    const pokeBox = $(".PokéBox");
-    const [first, second, third] = $(pokeBox).find("> tbody > tr").toArray();
-    const [lbs, kg, hInch, hMeters] = $(third).find("tr").eq(3).find('td').map((idx, el) => $(el).text().trim()).toArray();
+  const pokeBox = $(".PokéBox");
+  const [first, second, third] = $(pokeBox).find("> tbody > tr").toArray();
+  const [lbs, kg, hInch, hMeters] = $(third).find("tr").eq(3).find('td').map((idx, el) => $(el).text().trim()).toArray();
 
-    class Selectors {
-        get mainTrs() {
-            return { first, second, third }
-        }
-
-        get jp() {
-            let [kanji, jpName] = $(this.mainTrs.first).find("i").text().split(" ")
-            return { kanji, jpName }
-        }
-
-        get genders() {
-            let [male, female] = $(this.mainTrs.third).find("tr").eq(1).text().match(/[0-9]{1,2}.?[0-9]{1,2}/g);
-            return { male, female };
-        }
-
-        get weight() {
-            return { lbs, kg }
-        }
-
-        get heigth() {
-            return { hInch, hMeters };
-        }
-
-        get childTr() {
-            return {
-                second: $(this.mainTrs.second).find("tr"),
-                third: $(this.mainTrs.third).find("tr")
-            };
-        }
-
-        get nIndex() {
-            return _.first(/\d+/g.exec(this.childTr.second.eq(5).text()))
-        }
+  class Selectors {
+    get mainTrs() {
+      return { first, second, third }
     }
 
-    return new Selectors($);
+    get jp() {
+      let [kanji, jpName] = $(this.mainTrs.first).find("i").text().split(" ")
+      return { kanji, jpName }
+    }
+
+    get genders() {
+      let [male, female] = $(this.mainTrs.third).find("tr").eq(1).text().match(/[0-9]{1,2}.?[0-9]{1,2}/g);
+      return { male, female };
+    }
+
+    get weight() {
+      return { lbs, kg }
+    }
+
+    get heigth() {
+      return { hInch, hMeters };
+    }
+
+    get childTr() {
+      return {
+        second: $(this.mainTrs.second).find("tr"),
+        third: $(this.mainTrs.third).find("tr")
+      };
+    }
+
+    get nIndex() {
+      return _.first(/\d+/g.exec(this.childTr.second.eq(5).text()))
+    }
+  }
+
+  return new Selectors($);
 };
--- public/js/bootstrap.js
+++ public/js/bootstrap.js
@@ -1,32 +1,32 @@
 ReactDOM.render(
-    <Container />,
-    document.getElementById('app')
+  <Container />,
+  document.getElementById('app')
 );
 
 ((console) => {
 
-    console.save = (data, filename) => {
+  console.save = (data, filename) => {
 
-        if (!data) {
-            return console.error('Console.save: No data');
-        }
-
-        if(!filename) {
-            filename = 'pokemon';
-        }
-
-        if (typeof data === "object") {
-            data = JSON.stringify(data, undefined, 4)
-        }
-
-        var blob = new Blob([data], { type: 'text/json' }),
-            e = document.createEvent('MouseEvents'),
-            a = document.createElement('a')
-
-        a.download = `${filename}.json`
-        a.href = window.URL.createObjectURL(blob)
-        a.dataset.downloadurl = ['text/json', a.download, a.href].join(':')
-        e.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null)
-        a.dispatchEvent(e)
+    if (!data) {
+      return console.error('Console.save: No data');
     }
+
+    if (!filename) {
+      filename = 'pokemon';
+    }
+
+    if (typeof data === "object") {
+      data = JSON.stringify(data, undefined, 4)
+    }
+
+    var blob = new Blob([data], { type: 'text/json' }),
+      e = document.createEvent('MouseEvents'),
+      a = document.createElement('a')
+
+    a.download = `${filename}.json`
+    a.href = window.URL.createObjectURL(blob)
+    a.dataset.downloadurl = ['text/json', a.download, a.href].join(':')
+    e.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null)
+    a.dispatchEvent(e)
+  }
 })(console)
