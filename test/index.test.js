40eaad1cd71de72028ccc5f392571e68b44dcecd
Ronniery Borges Correa <ronnisti@gmail.com>
2020-05-14 18:22:53 -0300

    Improved testing with unit testing and coverage testing

--- test/index.test.js
+++ test/index.test.js
@@ -1,19 +1,48 @@
 /* eslint-disable no-undef */
-const chai = require('chai')
+const { expect } = require('chai')
+const { isObject, isEmpty, isArray } = require('lodash')
 const request = require('supertest')
 
-describe('Testing the root entry of the application', () => {
+describe('Test the root entry of the application', function () {
+  this.timeout(10000);
+
   let app;
-  
-  beforeEach(function () {
+
+  beforeEach(() => {
     app = require('../app')
   });
 
-  it('Should get /', done => {
+  it('Should get /.', done => {
     request(app)
       .get('/')
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(200, done)
+  })
+
+  it('Should get / to receive valid json response.', done => {
+    request(app)
+      .get('/')
+      .end((_err, { body: { availableRoutes, message } }) => {
+        expect(availableRoutes).to.be.not.null;
+        expect(isArray(availableRoutes)).to.be.true
+        expect(message).to.be.not.null;
+        expect(message).to.be.eq('The server is correctly running!')
+        done();
+      });
+  })
+
+  it('Should get a 404 for all not mapped routes.', done => {
+    const path = '/404'
+
+    request(app)
+      .get(path)
+      .end((_err, { statusCode, body, error }) => {
+        expect(statusCode).to.equal(404);
+        expect(isObject(body) && isEmpty(body)).to.be.true;
+        expect(error).to.be.not.null;
+        expect(error.message).to.be.eq(`cannot GET ${path} (404)`)
+        done();
+      });
   })
 })
