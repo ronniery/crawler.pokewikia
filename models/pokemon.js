b8bc69af5daecd0896c74db57fefc9b6243c5ccf
Ronniery Borges Correa <ronnisti@gmail.com>
2020-05-20 17:25:05 -0300

    Added doc for pokemon model

--- models/pokemon.js
+++ models/pokemon.js
@@ -2,12 +2,27 @@
 const _ = require('lodash')
 const mongoose = require('mongoose');
 
+/**
+ * Pokemon schema generator to create the schema and set the custom static methods.
+ *
+ * @class PokeSchema
+ */
 class PokeSchema {
 
+  /**
+   * Create and prepare the pokemon schema to be used.
+   *
+   * @returns {Schema<Pokemon>} Prepared pokemon schema.
+   * @memberof PokeSchema
+   */
   create() {
     const schema = mongoose.Schema({
       dexdata: {
-        name: { type: String, unique: true, index: true }
+        name: { 
+          type: String, 
+          unique: true, 
+          index: true 
+        }
       }
     }, {
       collection: 'pokemons',
@@ -20,6 +35,13 @@ class PokeSchema {
     return schema
   }
 
+  /**
+   * Save a document if it not exists on database.
+   *
+   * @param {any} doc Document to be saved.
+   * @returns {Promise<any>} The *findOne* promise resolver.
+   * @memberof PokeSchema
+   */
   async saveIfNotExits(doc) {
     const Model = this.model('Pokemon')
     const copy = _.cloneDeep(doc)
@@ -36,6 +58,13 @@ class PokeSchema {
     })
   }
 
+  /**
+   * Converts the mongoose object to json.
+   *
+   * @override
+   * @returns {Object} The converted and cleaned object without mongoose properties.
+   * @memberof PokeSchema
+   */
   toJSON() {
     const obj = this.toObject()
     delete obj.__v
@@ -44,12 +73,26 @@ class PokeSchema {
     return obj
   }
 
+  /**
+   * Set functions on `methods` mongoose property.
+   *
+   * @param {Schema<Pokemon>} schema Schema that will be injected the functions.
+   * @returns {Schema<Pokemon>} Schema with functions injected on the property `methods`.
+   * @memberof PokeSchema
+   */
   setMethods(schema) {
     const { methods } = schema
     methods.toJSON = this.toJSON
     return schema
   }
 
+  /**
+   * Set static functions on `statics` mongoose property.
+   *
+   * @param {Schema<Pokemon>} schema Schema that will be injected the functions.
+   * @returns {Schema<Pokemon>} Schema with functions injected on the property `statics`.
+   * @memberof PokeSchema
+   */
   setStatics(schema) {
     const { statics } = schema
     statics.saveIfNotExits = this.saveIfNotExits
