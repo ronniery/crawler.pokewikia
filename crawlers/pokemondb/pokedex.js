1a2147803025db789fff1fb785d6ec752df1bc75
Ronniery Borges Correa <ronnisti@gmail.com>
2020-05-20 17:16:24 -0300

    Added documentation for pokedex

--- crawlers/pokemondb/pokedex.js
+++ crawlers/pokemondb/pokedex.js
@@ -2,8 +2,24 @@ const _ = require('lodash');
 
 const Helpers = require('@crawlers/helpers')
 
+/**
+ * Get all pokedex data from pokedex data section.
+ *
+ * @class Pokedex
+ */
 class Pokedex {
 
+  /**
+   * Create the full pokedex object representation.
+   *
+   * @static
+   * @public
+   * @param {Function} cheerio Function with page as `Cheerio` library reference.
+   * @param {CheerioElement} anchor An element that pointers to the current anchor
+   * that will be used instead the entire `document`
+   * @returns {any} The reference of pokedex object with new pokedex line entries.
+   * @memberof Pokedex
+   */
   static getPokedex(cheerio, anchor) {
     const $ = cheerio();
     const found = Helpers.searchTableOnDocument(cheerio, { 
@@ -15,16 +31,26 @@ class Pokedex {
 
     return $table
       .findArray('tr')
-      .reduce((reducer, tr) => {
+      .reduce((dexline, tableRow) => {
         return Object.assign(
-          reducer,
-          Pokedex._rowToPokedexLine(() => ({ $, tr }))
+          dexline,
+          Pokedex._rowToPokedexLine(() => ({ $, tr: tableRow }))
         );
       }, {
         name: $(anchor).text()
       });
   }
 
+  /**
+   * Convert the given elements in `domHandlers` to a pokedex line.
+   *
+   * @static
+   * @private
+   * @param {{$: Cheerio, tr: CheerioElement}} domHandlers The `Cheerio` reference to
+   * the entire document and the table row with pokemon data that will be parsed.
+   * @returns {any} The reference of pokedex object with new pokedex line entries.
+   * @memberof Pokedex
+   */
   static _rowToPokedexLine(domHandlers) {
     const dexline = {}
     const { nextProp, nextValue } = Pokedex._createParseableElements(
@@ -39,6 +65,19 @@ class Pokedex {
     return dexline;
   }
 
+  /**
+   * Organize on a simple way with next property name that will be used as a property name
+   * on pokedex line object and your value on next value object property, with that we can
+   * start parse the elements to extract a poke dexline.
+   *
+   * @static
+   * @private
+   * @param {{$: Cheerio, tr: CheerioElement}} domHandlers The `Cheerio` reference to
+   * the entire document and the table row with pokemon data that will be parsed.
+   * @returns {{ nextProp: String, nextValue: String }} The parseable content based on
+   * given handlers.
+   * @memberof Pokedex
+   */
   static _createParseableElements(domHandlers) {
     const getValueFrom = selector => {
       const { $, tr } = domHandlers();
@@ -55,6 +94,18 @@ class Pokedex {
     };
   }
 
+  /**
+   * Parse the `localNº` from pokedex section.
+   *
+   * @static
+   * @private
+   * @param {Object} dexline Current pokedex line.
+   * @param {String} nextProperty A checker that will determine when run that function,
+   * triggering this code only when the property 'local№' is reached.
+   * @param {String} nextValue The value to be cleaned and set on the final property
+   * `localizations`.
+   * @memberof Pokedex
+   */
   static _getLocalizations(dexline, nextProperty, nextValue) {
     if (nextProperty === 'local№') {
       // Prevent useless property on dexline
@@ -75,10 +126,22 @@ class Pokedex {
     return dexline;
   }
 
+  /**
+   * Parse the abilities from pokedex section.
+   *
+   * @static
+   * @private
+   * @param {Object} dexline Current pokedex line.
+   * @param {String} nextProperty A checker that will determine when run that function,
+   * triggering this code only when the property 'abilities' is reached.
+   * @param {{$: Cheerio, tr: CheerioElement}} domHandlers The `Cheerio` reference to
+   * the entire document and the table row with pokemon data that will be parsed.
+   * @memberof Pokedex
+   */
   static _getAbilities(dexline, nextProperty, domHandlers) {
-    const { $, tr } = domHandlers();
-
     if (nextProperty === 'abilities') {
+      const { $, tr } = domHandlers();
+
       dexline.abilities = $(tr).findArray('a')
         .map(el => $(el).text2());
     }
